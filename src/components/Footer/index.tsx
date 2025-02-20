0205f1da39b94c49aac515977054a5d5b3664521
ShinRunner1st <98098151+ShinRunner1st@users.noreply.github.com>
2025-02-21 01:42:03 +0700

    add github link

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -6,7 +6,10 @@ import * as Styled from "./index.styled";
 export function Footer() {
   return (
     <Styled.Text>
-      Made with <IoHeart /> by ShinRunner1st
+      Made with <IoHeart /> by{" "}
+        <Styled.Link href="https://github.com/ShinRunner1st/ba_ost_guess">
+          ShinRunner1st
+        </Styled.Link>
     </Styled.Text>
   );
 }
