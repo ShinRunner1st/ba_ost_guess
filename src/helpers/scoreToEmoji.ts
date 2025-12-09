93e092f5037b437012d7685e07c15ad24b08cef7
ShinRunner1st <shinrunner1st@gmail.com>
2025-12-10 05:10:05 +0700

    fix: correct order of stats display in scoreToEmoji function

--- src/helpers/scoreToEmoji.ts
+++ src/helpers/scoreToEmoji.ts
@@ -34,13 +34,13 @@ export function scoreToEmoji(
   const total = Stats[7];
   const guessIn =
     "Guessed in\n" +
-    "1: " + Stats[1] + " " + percentToGreenBlocks(Stats[1], total) + "\n" +
-    "2: " + Stats[2] + " " + percentToGreenBlocks(Stats[2], total) + "\n" +
-    "3: " + Stats[3] + " " + percentToGreenBlocks(Stats[3], total) + "\n" +
-    "4: " + Stats[4] + " " + percentToGreenBlocks(Stats[4], total) + "\n" +
-    "5: " + Stats[5] + " " + percentToGreenBlocks(Stats[5], total) + "\n" +
-    "6: " + Stats[6] + " " + percentToGreenBlocks(Stats[6], total) + "\n" +
-    "X: " + Stats[0] + " " + redBlocks(Stats[0], total);
+    "1: " + percentToGreenBlocks(Stats[1], total) + " " + Stats[1] + "\n" +
+    "2: " + percentToGreenBlocks(Stats[2], total) + " " + Stats[2] + "\n" +
+    "3: " + percentToGreenBlocks(Stats[3], total) + " " + Stats[3] + "\n" +
+    "4: " + percentToGreenBlocks(Stats[4], total) + " " + Stats[4] + "\n" +
+    "5: " + percentToGreenBlocks(Stats[5], total) + " " + Stats[5] + "\n" +
+    "6: " + percentToGreenBlocks(Stats[6], total) + " " + Stats[6] + "\n" +
+    "X: " + redBlocks(Stats[0], total) + " " + Stats[0];
 
   let scoreEmoji = "";
 
